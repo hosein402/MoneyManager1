47ef30026e490d4af42dea5dd69fdf226bf37a24
hosein402 <Hosein.narvan@gmail.com>
2026-08-16 06:06:56 +0330

    Create AppDatabase.java

---
+++ app/src/main/java/ir/moneymanager/app/db/AppDatabase.java
@@ -0,0 +1,35 @@
+package ir.moneymanager.app.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {TransactionEntity.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static volatile AppDatabase INSTANCE;
+
+    public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(2);
+
+    public abstract TransactionDao transactionDao();
+
+    public static AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                                    context.getApplicationContext(),
+                                    AppDatabase.class,
+                                    "money_manager_db")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+                      }
