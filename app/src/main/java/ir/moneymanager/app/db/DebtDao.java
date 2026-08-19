a20b1aaa3d670ac4c74a174a8e4f7beb4589bedb
hosein402 <Hosein.narvan@gmail.com>
2026-08-19 04:54:33 +0330

    Update DebtDao.java

--- app/src/main/java/ir/moneymanager/app/db/DebtDao.java
+++ app/src/main/java/ir/moneymanager/app/db/DebtDao.java
@@ -15,11 +15,17 @@ public interface DebtDao {
     @Query("SELECT * FROM debts WHERE type = :type ORDER BY isSettled ASC, date DESC")
     List<DebtEntity> getByType(String type);
 
+    @Query("SELECT * FROM debts")
+    List<DebtEntity> getAllDebts();
+
     @Query("UPDATE debts SET isSettled = 1 WHERE id = :id")
     void markSettled(int id);
 
     @Query("DELETE FROM debts WHERE id = :id")
     void delete(int id);
+
+    @Query("DELETE FROM debts")
+    void deleteAll();
 
     @Query("SELECT * FROM debts WHERE id = :id")
     DebtEntity getById(int id);
